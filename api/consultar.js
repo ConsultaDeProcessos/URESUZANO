a0a905d1993a633d6d19d00fcecf79597b67674d
danielaresende26 <projetovibecode@gmail.com>
2026-03-25 16:23:21 -0300

    Update consultar.js

--- api/consultar.js
+++ api/consultar.js
@@ -61,7 +61,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Turnstile-Token');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
